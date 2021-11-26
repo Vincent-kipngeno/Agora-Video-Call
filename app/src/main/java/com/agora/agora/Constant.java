e92929b24eb58f18fde41ba6d0f9ecb1644fef26
Vincent-kipngeno <kipngenovi2001@gmail.com>
2021-11-26 19:07:22 +0300

    Create Video Call App using agora sdk

---
+++ app/src/main/java/com/agora/agora/Constant.java
@@ -0,0 +1,11 @@
+package com.agora.agora;
+
+import android.Manifest;
+
+public class Constant {
+
+    // Permissions
+    public static final int PERMISSION_REQ_ID = 22;
+    public static final String[] REQUESTED_PERMISSIONS = {Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA};
+
+}
